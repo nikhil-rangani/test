002aa26c1d863fb979cec358c74f5d5ca99b2c3e
nikhil rangani <79688822+nikhil-rangani@users.noreply.github.com>
2022-05-27 18:38:01 +0530

    till if chenges

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { bufferToggle } from 'rxjs';
+
+@Component({
+  //selector: 'app-test',<app-test>
+  //selector: '.app-test', class="app-test"
+  selector: '[app-test]', 
+  templateUrl:'/test.component.html',
+  styles: [`p{
+             color:red
+          }
+          .abc{
+            color:blue;
+          }
+          .dfg{
+            font-style: italic;
+          }`]
+})
+export class TestComponent implements OnInit {
+
+  constructor() { }
+  public testdata="chiled component ";
+  isdisble=true;
+  idname="testid";
+  classname="abc";
+  cssappy=false;
+   multiclass={
+    "abc" : true,
+    "dfg" : true
+  }
+  stylecolor="yellow";
+  multistyle={
+    color:"green",
+    fontStyle: "italic"
+  }
+  
+
+  ngOnInit(): void {
+  }
+  showref="";
+  funrefdata(refdara : any){
+  console.log(refdara);    
+  this.showref= refdara.value;
+  }
+  empname="";
+  condition= true
+}
