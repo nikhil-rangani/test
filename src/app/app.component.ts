c8b7e6f3ecba214f1cdccaae5c7878ae28afdef4
nikhil rangani <79688822+nikhil-rangani@users.noreply.github.com>
2022-05-27 18:46:25 +0530

    asd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../environments/environment.prod';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'test';
+export class AppComponent implements OnInit {
+
+  public title : string = "nikhil";
+  total = this.title.length;
+  location= window.location.href;
+  constructor(){}
+  ngOnInit(): void {
+    
+  }
+  btndata="";
+  btnclick(event : any ){
+    
+    console.log(event);
+    this.btndata=event.type;
+  }  
+  getname(){
+    return "my name "+ this.title;
+  }
 }
